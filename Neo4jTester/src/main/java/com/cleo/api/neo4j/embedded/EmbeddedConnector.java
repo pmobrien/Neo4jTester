f8522a9f7a62a1a12a3493ba91ce89a09569124b
pmobrien <pobrien@cleo.com>
2017-03-02 16:16:39 -0600

    initial attempt at hacking some ha properties in

--- Neo4jTester/src/main/java/com/cleo/api/neo4j/embedded/EmbeddedConnector.java
+++ Neo4jTester/src/main/java/com/cleo/api/neo4j/embedded/EmbeddedConnector.java
@@ -3,6 +3,14 @@ package com.cleo.api.neo4j.embedded;
 import com.cleo.api.neo4j.Utils;
 import com.cleo.api.neo4j.pojo.NeoEntity;
 import com.cleo.api.neo4j.pojo.Person;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.common.collect.Maps;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.neo4j.ogm.config.Configuration;
@@ -11,19 +19,64 @@ import org.neo4j.ogm.session.SessionFactory;
 
 public class EmbeddedConnector {
   
+  private static final String NEO_HA_PROPERTIES = "neo4j-ha.properties";
+  
+  private static final Supplier<Path> NEO_STORE_PATH = Suppliers.memoize(() -> initializeNeoStorePath());
+  private static final Supplier<Path> NEO_HA_PROPERTIES_PATH = Suppliers.memoize(() -> initializeHaProperties());
+  private static final Supplier<SessionFactory> SESSION_FACTORY = Suppliers.memoize(() -> initializeSessionFactory());
+  
+  private static Path initializeNeoStorePath() {
+    Path path = Paths.get(Paths.get("").toAbsolutePath().toString(), "neo-store");
+    path.toFile().mkdir();
+    
+    return path;
+  }
+  
+  private static Path initializeHaProperties() {
+    Path path = Paths.get(NEO_STORE_PATH.get().toString(), NEO_HA_PROPERTIES);
+    
+    try {
+      path.toFile().createNewFile();
+
+      try(PrintWriter writer = new PrintWriter(path.toFile())) {
+        writer.write(
+            new StringBuilder()
+                .append("ha.server_id=1").append(System.lineSeparator())
+                .append("ha.initial_hosts=localhost:5001").append(System.lineSeparator())
+                .append("ha.allow_init_cluster=true").append(System.lineSeparator())
+                .toString()
+        );
+      }
+
+      Method addUrl = URLClassLoader.class.getDeclaredMethod("addURL", new Class[] { URL.class });
+      addUrl.setAccessible(true);
+      addUrl.invoke(ClassLoader.getSystemClassLoader(), new Object[] { NEO_STORE_PATH.get().toFile().toURI().toURL() });
+    } catch(IOException | ReflectiveOperationException ex) {
+      ex.printStackTrace(System.out);
+    }
+    
+    return path;
+  }
+  
+  private static SessionFactory initializeSessionFactory() {
+    Configuration configuration = new Configuration(NEO_HA_PROPERTIES_PATH.get().getFileName().toString());
+    
+    configuration.driverConfiguration()
+        .setURI(NEO_STORE_PATH.get().toUri().toString())
+        .setDriverClassName("org.neo4j.ogm.drivers.embedded.driver.EmbeddedDriver");
+
+    return new SessionFactory(configuration, "com.cleo.api.neo4j.pojo");
+  }
+  
+  public static Session openSession() {
+    return SESSION_FACTORY.get().openSession();
+  }
+  
   public static void main(String[] args) {
     try {
-      Path path = Paths.get(Paths.get("").toAbsolutePath().toString(), "neo-store");
-      path.toFile().mkdir();
+      Session session = openSession();
       
-      Configuration configuration = new Configuration();
-      configuration.driverConfiguration()
-          .setURI(path.toUri().toString())
-          .setDriverClassName("org.neo4j.ogm.drivers.embedded.driver.EmbeddedDriver");
-
-      SessionFactory sessionFactory = new SessionFactory(configuration, "com.cleo.api.neo4j.pojo");
-
-      Session session = sessionFactory.openSession();
+      session.query("MATCH (n) DETACH DELETE n", Maps.newHashMap());
       
       session.save(
           NeoEntity.newEntity(Person.class)
